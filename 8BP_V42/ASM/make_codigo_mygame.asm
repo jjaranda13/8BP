3bdee603e3ff8c6f70253e46c6951cb4e81961e9
jjaranda13 <jjaranda13@gmail.com>
2024-09-01 18:55:55 +0200

    V42_001

--- 8BP_V42/ASM/make_codigo_mygame.asm
+++ 8BP_V42/ASM/make_codigo_mygame.asm
@@ -3,7 +3,8 @@
 ;read "8bitsDePoder_v041_001.asm";
 ;read "8bitsDePoder_v041_002.asm";
 ;read "8bitsDePoder_v041_003.asm";
-read "8bitsDePoder_v042_000.asm";
+;read "8bitsDePoder_v042_000.asm";
+read "8bitsDePoder_v042_001.asm";
 ;---------------------PLAYER DE MUSICA-------------------------
 ; lo cargo justo despues de las rutinas. 
 ; rutinas + player no llega a ocupar 5KB.
